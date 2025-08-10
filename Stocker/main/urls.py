36bb7302f46956a64324bd39cf25b6a92d2c219f
SarahAbdulmajeed <sarahalsmari1999@gmail.com>
2025-08-10 06:05:48 +0300

    Complete Product Type managment and Supplier managment

--- Stocker/main/urls.py
+++ Stocker/main/urls.py
@@ -6,9 +6,21 @@ app_name = "main"
 urlpatterns = [ 
     path('', views.home_view, name='home_view'),
     path('dashboard/', views.dashboard_view, name='dashboard_view'),
-    path('products/', views.all_products_view, name='all_products_view'),
+    # Product
+    path('products/', views.products_view, name='products_view'),
+    path('products/add/', views.add_product, name='add_product'),
+    path('products/edit/<product_id>', views.edit_product, name='edit_product'),
+    path('products/delete/<product_id>', views.delete_product, name='delete_product'),
+
+    # Category
     path('categories/', views.categories_view, name='categories_view'),
     path('categories/add/', views.add_category, name='add_category'),
     path('categories/edit/<category_id>', views.edit_category, name='edit_category'),
     path('categories/delete/<category_id>', views.delete_category, name='delete_category'),
+    # Supplier
+    path('suppliers/', views.suppliers_view, name='suppliers_view'),
+    path('suppliers/add/', views.add_supplier, name='add_supplier'),
+    path('suppliers/edit/<supplier_id>', views.edit_supplier, name='edit_supplier'),
+    path('suppliers/delete/<supplier_id>', views.delete_supplier, name='delete_supplier'),
+   
 ] 
--- Stocker/main/views.py
+++ Stocker/main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse 
 from django.contrib import messages
 from django.contrib.auth.decorators	import login_required
-from .models import Category, Supplier
-from .forms import CategoryForm
+from .models import Category, Supplier, Product
+from .forms import CategoryForm, SupplierForm, ProductForm
 from django.core.paginator import Paginator 
 
 
@@ -17,10 +17,11 @@ def dashboard_view(request: HttpRequest):
 #===========[Category]===========
 def categories_view(request: HttpRequest):
 	categories = Category.objects.all()
+	total_categories = categories.count()
 	page_number = request.GET.get("page",1)
 	paginator = Paginator(categories,7)
 	categories_page = paginator.get_page(page_number)
-	return render(request, "main/category/all.html", {'categories':categories_page})
+	return render(request, "main/category/all.html", {'categories':categories_page, 'total_categories':total_categories})
 
 def add_category(request: HttpRequest):
 	if request.method == "POST":
@@ -53,46 +54,86 @@ def delete_category(request: HttpRequest, category_id):
 		return redirect("main:categories_view")
 	return render(request, "main/category/delete.html",{"category":category})
 
-
 #===========[Supplier]===========
-def Suppliers_view(request: HttpRequest):
+def suppliers_view(request: HttpRequest):
 	suppliers = Supplier.objects.all()
+	total_suppliers = suppliers.count()
 	page_number = request.GET.get("page",1)
 	paginator = Paginator(suppliers,7)
 	suppliers_page = paginator.get_page(page_number)
-	return render(request, "main/category/all.html", {'suppliers':suppliers_page})
+	return render(request, "main/supplier/all.html", {'suppliers':suppliers_page, 'total_suppliers':total_suppliers})
 
 def add_supplier(request: HttpRequest):
 	if request.method == "POST":
-		category_form = CategoryForm(request.POST)
-		if category_form.is_valid():
-			category_form.save()
-			messages.success(request, "Created Category Successfully!")
-			return redirect('main:categories_view')
+		supplier_form = SupplierForm(request.POST)
+		if supplier_form.is_valid():
+			supplier_form.save()
+			messages.success(request, "Created Supplier Successfully!")
+			return redirect('main:suppliers_view')
 		else:
-			print(category_form.errors)
-	return render(request, "main/category/add.html")
+			print(supplier_form.errors)
+	return render(request, "main/supplier/add.html")
 
-def edit_supplier(request: HttpRequest, category_id):
-	category = Category.objects.get(pk=category_id)
+def edit_supplier(request: HttpRequest, supplier_id):
+	supplier = Supplier.objects.get(pk=supplier_id)
 	if request.method == "POST":
-		category_form = CategoryForm(request.POST, instance=category)
-		if category_form.is_valid():
-			category_form.save()
-			messages.success(request, "Edit Category Successfully!")
-			return redirect("main:categories_view")
+		supplier_form = SupplierForm(request.POST, instance=supplier)
+		if supplier_form.is_valid():
+			supplier_form.save()
+			messages.success(request, "Edit Supplier Successfully!")
+			return redirect("main:suppliers_view")
 		else:
-			print(category_form.errors)
-	return render(request, "main/category/edit.html",{"category":category})
+			print(supplier_form.errors)
+	return render(request, "main/supplier/edit.html",{"supplier":supplier})
 
-def delete_supplier(request: HttpRequest, category_id):
-	category = Category.objects.get(pk=category_id)
+def delete_supplier(request: HttpRequest, supplier_id):
+	supplier = Supplier.objects.get(pk=supplier_id)
 	if request.method == "POST":
-		category.delete()
-		messages.success(request, "Category Deleted Successfully!")
-		return redirect("main:categories_view")
-	return render(request, "main/category/delete.html",{"category":category})
+		supplier.delete()
+		messages.success(request, "Supplier Deleted Successfully!")
+		return redirect("main:suppliers_view")
+	return render(request, "main/supplier/delete.html",{"supplier":supplier})
 
 #===========[Product]===========
-def all_products_view(request: HttpRequest):
-	return render(request, "main/products.html")
+def products_view(request: HttpRequest):
+	products = Product.objects.all()
+	total_products = products.count()
+	page_number = request.GET.get("page",1)
+	paginator = Paginator(products,7)
+	products_page = paginator.get_page(page_number)
+	return render(request, "main/product/all.html", {'products':products_page, 'total_products':total_products})
+
+def add_product(request: HttpRequest):
+	categories = Category.objects.all()
+	if request.method == "POST":
+		product_form = ProductForm(request.POST)
+		if product_form.is_valid():
+			product_form.save()
+			messages.success(request, "Created Product Successfully!")
+			return redirect('main:products_view')
+		else:
+			messages.error(request, "Please fix the errors below.")
+			print(product_form.errors)
+	return render(request, "main/product/add.html", {'categories':categories})
+
+def edit_product(request: HttpRequest, product_id):
+	categories = Category.objects.all()
+	product = Product.objects.get(pk=product_id)
+	if request.method == "POST":
+		product_form = ProductForm(request.POST, instance=product)
+		if product_form.is_valid():
+			product_form.save()
+			messages.success(request, "Edit Product Successfully!")
+			return redirect("main:products_view")
+		else:
+			print(product_form.errors)
+	return render(request, "main/product/edit.html",{"product":product, 'categories':categories})
+
+def delete_product(request: HttpRequest, product_id):
+	categories = Category.objects.all()
+	product = Product.objects.get(pk=product_id)
+	if request.method == "POST":
+		product.delete()
+		messages.success(request, "Product Deleted Successfully!")
+		return redirect("main:products_view")
+	return render(request, "main/product/delete.html",{"product":product, 'categories':categories})
